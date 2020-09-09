faec609d5e9880194ce9ca7bd230fa56c86e46d4
gromuxall <smadon3@uic.edu>
2020-09-08 19:32:55 -0500

    Address some courses not loading lecture list
    Insert a try/catch to see if class-row contains a media capture button,
    as some courses would neglect this when there was no class and my
    wait check for number of lectures would fail as these were not accounted
    for.

--- lhook_app.py
+++ lhook_app.py
@@ -7,7 +7,6 @@
 
 '''
 import sys
-import time
 import os
 import os.path
 import calendar as cal
@@ -27,17 +26,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 
 from waits import elements_with_xpath, elements_by_length
 from js import CLICK_ALL_FUNC, CLICK_ALL_BTNS, CLICK_ALL_LINKS, FILTER_LIST_FUNC
-
-
-class ElementNotFound(Exception):
-    '''Exception raised for error in finding element'''
-    def __init__(self, element, message="Element not found"):
-        self.element = element
-        self.message = message
-        super().__init__(self.message)
 
 
 class Quality(Enum):
@@ -124,24 +116,19 @@ class Course():
     # <Course> ------------------------------------------------------------- //
     @staticmethod
     def sort_courses():
-        '''
-        Sort the static variable list courses
-        '''
+        '''Sort the static variable list courses'''
         Course.courses = sorted(Course.courses, key=lambda x: x.crs_num)
 
     # <Course> ------------------------------------------------------------- //
     @staticmethod
     def split_link(string):
-        '''
-        Utility that splits off url from accompanying information
-        '''
+        '''Utility that splits off url from accompanying information'''
         return string.get_attribute('value').split(' || ')[0]
 
     # <Course> ------------------------------------------------------------- //
     @staticmethod
     def check_dir(path):
-        '''
-        Check if directory exists
+        '''Check if directory exists
 
         True:
            changes to chosen directory
@@ -155,44 +142,32 @@ class Course():
         except IOError as err:
             if err.errno == errno.EEXIST:
                 os.chdir(path)
-        print('Downloading to {}'.format(path)) #TODO change this
 
     # <Course> ------------------------------------------------------------- //
     def short_name(self):
-        '''
-        Return short string representation of course
-        '''
+        '''Return short string representation of course'''
         return '{}{}'.format(self.crs_code, self.crs_num)
 
     # <Course> ------------------------------------------------------------- //
     def long_name(self):
-        '''
-        Return long string representation of course
-        '''
+        '''Return long string representation of course'''
         return '{} - {}'.format(self.short_name(), self.title)
 
     # <Course> ------------------------------------------------------------- //
     def menu_line(self):
-        '''
-        Provides a pretty formatted string for the menu
-        '''
+        '''Provides a pretty formatted string for the menu'''
         return self.long_name().ljust(42) + (self.crn).ljust(20)
 
     # <Course> ------------------------------------------------------------- //
     def goto_course(self):
-        '''
-        Changes active driver window to chosen course
-        '''
+        '''Changes active driver window to chosen course'''
         DRIVER.get(self.url)
-        time.sleep(2) # TODO change this, add a wait here
-
         Course.check_dir(self.short_name())
         self.fill_lectures()
 
     # <Course> ------------------------------------------------------------- //
     def extract_links(self, selects):
-        '''
-        Extracts the SD and HD version links from options in the passed
+        '''Extracts the SD and HD version links from options in the passed
         in list of selects and fills those values in for the video objects
         '''
         for sel, vid in zip(selects, self.lectures):
@@ -202,21 +177,26 @@ class Course():
 
     # <Course> ------------------------------------------------------------- //
     def fill_lectures(self):
-        '''
-        Get lecture elements
-        '''
+        '''Get lecture elements'''
         wait = WebDriverWait(DRIVER, 10)
         rows = wait.until(
             elements_with_xpath("//div[@class='class-row']"))
 
         for idx, row in enumerate(rows):
-            self.lectures.append(Video(
-                self,
-                row.find_element_by_class_name('header').text,
-                row.find_element_by_xpath(".//span[@class='date']").text,
-                idx))
+            try:
+                row.find_element_by_xpath(
+                    ".//*[contains(@class, 'courseMediaIndicator capture')]")
+                self.lectures.append(Video(
+                    self,
+                    row.find_element_by_class_name('header').text,
+                    row.find_element_by_xpath(".//span[@class='date']").text,
+                    idx))
+            except NoSuchElementException:
+                LOGGER.info('Element not found')
+                pass
 
         num_lecs = len(self.lectures)
+        LOGGER.info('num_lecs = {}'.format(num_lecs))
 
         # find and click all btns to bring up video options
         DRIVER.execute_script('{}{}'.format(CLICK_ALL_FUNC, CLICK_ALL_BTNS))
@@ -242,6 +222,7 @@ class Course():
         qty_choice = TerminalMenu(menu_entries=['SD', 'HD'],
                                   title='Quality').show()
 
+        print('Downloading to {}/{}'.format(App.get('root'), self.short_name()))
         if lec_choice == 0: # Chose 'All Videos'
             with concurrent.futures.ThreadPoolExecutor(
                     max_workers=3) as executor:
@@ -251,16 +232,15 @@ class Course():
                     tasks.append(Task(vid, qty_choice))
                 for task in tasks:
                     futures.append(executor.submit(task.download))
-                #for ret in concurrent.futures.as_completed(futures):
-                #    #tiasks[ret].finish_
+                for _ in concurrent.futures.as_completed(futures):
+                    #tasks[ret].finish_msg()
+                    pass
         else:
             Task(self.lectures[lec_choice-1], qty_choice).download()
 
 
 class Video():
-    '''
-    Object representing a class-row of a lecture for a given course
-    '''
+    '''Object representing a class-row of a lecture for a given course'''
     cal_num = {name: str(num).zfill(2) for num, name in
                enumerate(cal.month_name) if num}
 
@@ -274,26 +254,21 @@ class Video():
 
     # <Video> -------------------------------------------------------------- //
     def get_content_len(self, quality):
-        '''
-        Returns integer content length of video
-        '''
+        '''Returns integer content length of video'''
         res = DRIVER.request('HEAD', self.links[Quality(quality).name])
         return int(res.headers['Content-Length'])
 
     # <Video> -------------------------------------------------------------- //
     def url(self, num):
-        '''
-        Return link for specified quality
-        '''
+        '''Return link for specified quality'''
         return self.links[Quality(num).name]
 
     # <Video> -------------------------------------------------------------- //
     def get_date(self):
-        '''
-        Returns string with numeric date
+        '''Returns string with numeric date
 
         Example:
-        August 29, 2019 => 08-29-2019
+            August 29, 2019 => 08-29-2019
         '''
         splits = self.date.replace(',', ' ').split()
         return '{}-{}-{}'.format(Video.cal_num[splits[0]],
@@ -301,17 +276,14 @@ class Video():
 
     # <Video> -------------------------------------------------------------- //
     def vid_title(self):
-        '''
-        Return formatted video title
-        '''
+        '''Return formatted video title'''
         crs = self.course
         return '{}{}-lec{}_{}'.format(crs.crs_code, crs.crs_num,
                                       str(self.index).zfill(2), self.get_date())
 
 
 class Task():
-    '''
-    Object for initializing progress bar for downloads and performing
+    '''Object for initializing progress bar for downloads and performing
     the download upon call
     '''
     def __init__(self, vid, quality):
@@ -327,17 +299,13 @@ class Task():
 
     # <Task> --------------------------------------------------------------- //
     def finish_msg(self):
-        '''
-        Display confirmation message
-        '''
+        '''Display confirmation message'''
         self.pbar.display(msg='{} downloaded.'.format(self.vid.vid_title()),
                           pos=self.vid.index)
 
     # <Task> --------------------------------------------------------------- //
     def download(self):
-        '''
-        Stream downloads file pointed to by self.url and returns index
-        '''
+        '''Stream downloads file pointed to by self.url and returns index'''
         with DRIVER.request('GET', self.url, stream=True) as res:
             res.raise_for_status()
 
@@ -345,8 +313,8 @@ class Task():
                 for chunk in res.iter_content(chunk_size=8192):
                     self.pbar.update(1)
                     file.write(chunk)
-                self.finish_msg()
-        return self.vid.index
+                #self.finish_msg()
+        #return self.vid.index
 
 
 
@@ -367,9 +335,7 @@ def fill_courses():
 
 # -------------------------------------------------------------------------- //
 def print_menu():
-    '''
-    Sort and print course list, then return choice
-    '''
+    '''Sort and print course list, then return choice'''
     course_names = [z.menu_line() for z in Course.courses]
     choice = TerminalMenu(course_names).show()
     Course.courses[choice].goto_course()
@@ -377,9 +343,7 @@ def print_menu():
 
 # -------------------------------------------------------------------------- //
 def check_root_path():
-    '''
-    Checks if path exists and changes to that directory if so
-    '''
+    '''Checks if path exists and changes to that directory if so'''
     # TODO: Check yaml file for root path, also, loop until root
     #       path is a valid one
     if not os.path.isdir(ARGS.root):
@@ -411,25 +375,14 @@ def load_session():
         password = DRIVER.find_element_by_xpath("//input[@id='password']")
         password.send_keys(PASS)
         password.submit()
-
-
-# -------------------------------------------------------------------------- //
-def save_state(config):
-    '''
-    Loads previous state of program
-    '''
-    with open('config.yaml', 'w') as fp:
-        yaml.dump(config, fp)
+        LOGGER.info(e)
 
 
 # -------------------------------------------------------------------------- //
 def main():
-    '''
-    Main program
-    '''
+    '''Main program'''
     print(MESSAGES['intro'], MESSAGES['root'])
     load_session()
-    #launch_page_echo()
     fill_courses()
     print_menu()
 
@@ -448,7 +401,7 @@ if __name__ == "__main__":
     # TODO: if no root path
     check_root_path()
     LOGGER = logging.getLogger('lhook_logger')
-    
+
     OPTIONS = Options()
 
     if not ARGS.window:
@@ -460,7 +413,7 @@ if __name__ == "__main__":
     PREFS = {'prompt_for_download': False}
     OPTIONS.add_experimental_option('prefs', PREFS)
     DRIVER = Chrome(DVR_PATH, options=OPTIONS)
-    
+
 
     # msg dictionary for laziness
     MESSAGES = {
