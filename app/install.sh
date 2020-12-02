ed0278345eb4fe2b4aa6d9b5581b591f43a772ea
gromuxall <smadon3@uic.edu>
2020-12-01 23:13:20 -0600

    run.sh is now created programatically

--- app/install.sh
+++ app/install.sh
@@ -6,7 +6,20 @@ source env/bin/activate
 pip3 install -r requirements.txt
 deactivate
 
-chmod 755 run.sh
 chmod 755 lhook_app.py
+
+exec 3<> run.sh
+
+echo "#!/bin/bash" >&3
+echo "" >&3
+echo "DIR=${PWD}" >&3
+echo "" >&3
+echo "source ${DIR}/env/bin/activate " >&3
+echo "python3 ${DIR}/lhook_app.py" >&3
+echo "deactivate"
+
+exec 3>&-
+
+chmod 755 run.sh
 
 echo "alias lecturehook='${PWD}/run.sh'" >> ~/.bashrc
