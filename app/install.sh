46695ce7bc23ac5e07afab8f913000e5d26e05ed
gromuxall <smadon3@uic.edu>
2020-12-01 22:56:04 -0600

    Added install and run scripts

---
+++ app/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python3 -m venv env
+source env/bin/activate
+pip3 install -r requirements.txt
+deactivate
+
+chmod 755 run.sh
+chmod 755 lhook_app.py
+
+echo "alias lecturehook='${PWD}/run.sh'" >> ~/.bashrc
---
+++ app/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source env/bin/activate
+python3 lhook_app.py
+deactivate
